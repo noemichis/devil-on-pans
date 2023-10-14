3529d20218a76bbef434e1179a7427e887468708
noemichis <noemi_chis@yahoo.com>
2023-10-14 08:47:33 +0000

    Register Catering app admin and fix indentation

--- catering/models.py
+++ catering/models.py
@@ -35,7 +35,7 @@ class Item(models.Model):
     """
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL
-      )
+    )
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
@@ -43,7 +43,7 @@ class Item(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True
-      )
+    )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     allergens = models.ManyToManyField(Allergen, blank=True)
