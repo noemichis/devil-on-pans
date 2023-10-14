dc39f56d5dd0112fe9884eb7e40bae0e2d6c9c1f
noemichis <noemi_chis@yahoo.com>
2023-10-14 09:07:03 +0000

    Create/register Item and Category Admin classes

--- catering/admin.py
+++ catering/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from .models import Item, Category, Allergen
 
 
-admin.site.register(Item)
-admin.site.register(Category)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'sku',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Allergen)
