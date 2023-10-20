02673b6ba71ad6d5cd9436c97e9aacfea7e26067
noemichis <noemi_chis@yahoo.com>
2023-10-20 17:34:22 +0000

    Create categories_context processor to loop through nav

---
+++ catering/contexts.py
@@ -0,0 +1,11 @@
+from .models import Category
+
+
+def categories_context(request):
+    categories = Category.objects.all
+
+    context = {
+        'categories': categories,
+    }
+
+    return context
