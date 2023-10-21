a1398306cfee4765b8cbfa3670166b4f633a1fb1
noemichis <noemi_chis@yahoo.com>
2023-10-21 13:34:03 +0000

    Added bag_tools+fix subtotal/replace jQuery with full version

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from catering.models import Item
 
 
@@ -40,3 +40,14 @@ def adjust_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    bag = request.session.get('bag', {})
+
+    bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
