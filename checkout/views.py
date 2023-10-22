e62b74ced210e8b6ef12bca170845346fe2c7b83
noemichis <noemi_chis@yahoo.com>
2023-10-22 16:13:45 +0000

    Added checkout templates/wire up URL+cripsy-bootsrap5

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    # stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    # stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
+
+        if not cart:
+            messages.error(request,
+                           "Your cart is currently empty"
+                           )
+            return redirect(reverse('paintings'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
