7a3a60b609cdcbae8587fbf5b079876440f64fc0
noemichis <noemi_chis@yahoo.com>
2023-12-03 10:09:58 +0000

    Added hire package model+admin register

--- hire/admin.py
+++ hire/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import HirePackage
 
-# Register your models here.
+
+class HirePackageAdmin(admin.ModelAdmin):
+    list_display = (
+        'package_name',
+        'price',
+        'image',
+        'serves_nr_guests',
+        'description',
+    )
+
+    ordering = ('package_name')
+
+    search_fields = ('package_name', 'price', 'serves_nr_guests', 'description')
+
+admin.site.register(HirePackage)
--- hire/models.py
+++ hire/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+# Creates chef hire package
+class HirePackage(models.Model):
+    """
+    Model for the Hire packages
+    """
+    package_name = models.CharField(max_length=254)
+    description = models.TextField()
+    serves_nr_guests = models.TextField(blank=True, null=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True,)
+
+    def __str__(self):
+        return self.package_name
